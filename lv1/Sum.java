15737a5d7529bc92c39af0c9858c1bca45ca2ee0
sungjiRyu <116089824+sungjiRyu@users.noreply.github.com>
2023-08-29 01:06:11 +0900

    operator

---
+++ lv1/Sum.java
@@ -0,0 +1,21 @@
+public class Sum {
+    public static void main(String[] args) {
+    Sum sum = new Sum();
+        System.out.println(sum.solution(1, 1));
+    }
+
+// 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+// 
+// 제한사항
+// -50,000 ≤ num1 ≤ 50,000
+// -50,000 ≤ num2 ≤ 50,000
+
+     int solution(int num1, int num2) {
+        int answer = 0;
+
+        if(-50000 <= num1 && num1 <= 50000 && -50000 <= num2 && num2 <= 50000){
+            answer = num1 + num2;
+        }
+        return answer;
+    }
+}
