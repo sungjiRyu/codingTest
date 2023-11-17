2756edcbc01b1d29dff309e7a8e06ae690a734ab
sungjiRyu <116089824+sungjiRyu@users.noreply.github.com>
2023-11-17 23:24:54 +0900

    가위바위보

---
+++ lv1/RockPaperScissors.java
@@ -0,0 +1,33 @@
+public class RockPaperScissors {
+
+    // 가위바위보
+
+    // 가위는 2 바위는 0 보는 5로 표현합니다. 
+    // 가위 바위 보를 내는 순서대로 나타낸 문자열 rsp가 매개변수로 주어질 때, 
+    // rsp에 저장된 가위 바위 보를 모두 이기는 경우를 순서대로 나타낸 문자열을 return하도록 solution 함수를 완성해보세요.
+
+    // 0 < rsp의 길이 ≤ 100
+    // rsp와 길이가 같은 문자열을 return 합니다.
+    // rsp는 숫자 0, 2, 5로 이루어져 있습니다.
+
+    public static void main(String[] args) {
+        System.out.println(solution("205"));
+        String rsp = "205";
+        String[] ArrayRsp = rsp.split("");
+        for(String temp : ArrayRsp)
+        System.out.println(temp);
+    }
+
+    static String solution(String rsp) {
+        String answer = "";
+        String[] ArrayRsp = rsp.split("");
+
+        for(String word : ArrayRsp){
+            if(word.equals("0")) answer += "5";
+            else if(word.equals("2"))  answer += "0";
+            else if(word.equals("5"))  answer += "2";
+        }
+
+        return answer;
+    }
+}
