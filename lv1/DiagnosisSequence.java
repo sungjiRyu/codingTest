f60a90b11f9659cbe193dd038b979d56cc20db39
ryu <ryu@gmil.com>
2023-11-19 23:42:06 +0900

    진료순서정하기(수정)

--- lv1/DiagnosisSequence.java
+++ lv1/DiagnosisSequence.java
@@ -23,4 +23,20 @@ class DiagnosisSequence {
        
         return answer;
     }
+
+    // public int[] solution(int[] emergency) {
+    //     int[] answer = new int[emergency.length];
+        
+    //     for (int i = 0; i < emergency.length; i++) {
+    //         for (int j = 0; j < emergency.length; j++) {
+    //             if (emergency[i] < emergency[j]) {
+    //                 answer[i]++;
+    //             }
+    //         }
+            
+    //         answer[i]++;
+    //     }
+        
+    //     return answer;
+    // }
 }
