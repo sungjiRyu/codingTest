60ce9bf56a532cbc475bc57128d66a068efa24de
sungjiRyu <116089824+sungjiRyu@users.noreply.github.com>
2023-12-20 18:09:13 +0900

    완주하지 못한 선수(HashMap)

--- Hash/DidntFinishPlayer1.java
+++ Hash/DidntFinishPlayer1.java
@@ -2,25 +2,34 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 // 프로그래머스 LV1 완주하지 못한 선수
-public class DidntFinishPlayer {
+public class DidntFinishPlayer1 {
     
     //participant = 마라톤에 참여한 선수들의 이름이 담긴 배열 
     //completion = 완주한 선수들의 이름이 담긴 배열
     public String solution(String[] participant, String[] completion) {
         // completion의 길이는 participant의 길이보다 1 작습니다.
         // 완주하지 못한 선수의 이름을 return
-        String answer = "null";
+        String answer = "";
         
-        Arrays.sort(participant);
-        Arrays.sort(completion);
-      
-        int i = 0;
-        for(; i < completion.length; i++ ){
-            if(!participant[i].equals(completion[i]))
-            break;
+        HashMap<String, Integer> map = new HashMap<>();
+
+        // 참가자 명단을 map에 넣는다.
+        for(String name : participant){
+            map.put(name, map.getOrDefault(name,0)+1);
         }
-        answer = participant[i];
-        
+
+        // 완주자 명단을 map에서 뺀다.
+        for(String name : completion){
+            map.put(name, map.get(name)-1);
+        }
+
+        // value 가 0이 아니면 완주하지 못한 선수
+        for(String key : map.keySet()){
+            if(map.get(key) != 0){
+                answer = key;
+                break;
+            }
+        }
         return answer;
     }
     
@@ -32,6 +41,7 @@ public class DidntFinishPlayer {
     
         System.out.println(sol.solution(participant, completion));
 
-    }
 }
 
+
+}
