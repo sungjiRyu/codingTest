78615eeafdaf99a5bfb29b259086fbf603c2eb51
sungjiRyu <dhftlcm@gmail.com>
2024-05-14 21:34:28 +0900

    배열의 유사도

---
+++ programmers/lv0/similarArray.java
@@ -0,0 +1,17 @@
+public class similarArray {
+    // 배열의 유사도
+    // 2024-05-14
+    public int solution(String[] s1, String[] s2){
+        int answer = 0;
+
+        for(int i = 0; i < s1.length; i++){
+            for(int j = 0; j < s2.length; j++){
+                if(s1[i].equals(s2[j])){
+                    answer ++;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
