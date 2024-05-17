fa631f8af95de9ef3263f44c2147b6208e385136
sungjiRyu <dhftlcm@gmail.com>
2024-05-17 23:53:19 +0900

    프로그래머스 대문자로 바꾸기

---
+++ programmers/lv0/ChangeUpperCase.java
@@ -0,0 +1,10 @@
+public class ChangeUpperCase {
+    // 대문자로 바꾸기
+    // 2024-05-17
+    public String solution(String myString) {
+        String answer = "";
+
+        answer = myString.toUpperCase();
+        return answer;
+    }
+}
