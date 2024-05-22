6027c5ea0ad47eeb2f7248bb24ed31270b51bb55
sungjiRyu <dhftlcm@gmail.com>
2024-05-22 23:32:36 +0900

    약수의 합

---
+++ programmers/lv1/SumOfDivisors.java
@@ -0,0 +1,19 @@
+public class SumOfDivisors {
+    // 약수의 합
+    // 2024-05-22
+    public static void main(String[] args) {
+        int answer = solution(5);
+        System.out.println(answer);
+    }
+    public static int solution(int n) {
+        int answer = 0;
+
+        for(int i = 1; i <= n; i++){
+            if(n%i == 0){
+                answer += i;
+            }
+        }
+
+        return answer;
+    }
+}
