72217f47688e5afefe37648a63ed1c1279e2ef97
sungjiRyu <dhftlcm@gmail.com>
2024-06-16 23:58:56 +0900

    문자열 다루기 기본

---
+++ programmers/lv1/StringHandling.java
@@ -0,0 +1,13 @@
+public class StringHandling {
+    // 문자열 다루기 기본
+    // 2024-06-16
+    public boolean solution(String s) {
+        boolean answer = true;
+
+        if(s.length() != 4 && s.length() != 6)
+            return false;
+
+        return s.matches("^[0-9]+$");
+
+    }
+}
