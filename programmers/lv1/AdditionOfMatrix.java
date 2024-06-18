faa558ca4204a099f9f1fd32a213a85940b02c6e
sungjiRyu <dhftlcm@gmail.com>
2024-06-18 23:40:22 +0900

    직사각형 별찍기

---
+++ programmers/lv1/AdditionOfMatrix.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdditionOfMatrix {
+    // 행렬의 덧셈
+    // 2024-06-17
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2.length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[i].length; j++) {
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+        return answer;
+    }
+}
---
+++ programmers/lv1/RectangleStar.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class RectangleStar {
+    // 직사각형 별찍기
+    // 2024-06-18
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for (int i = 0; i < b; i++) {
+            for (int j = 0; j < a; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
