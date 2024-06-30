c9c160e334bc6f5396331eba49cf7343664dfaea
sungjiRyu <dhftlcm@gmail.com>
2024-06-30 23:36:35 +0900

    제일 작은 수 제거하기

--- programmers/lv1/RemoveSmallestNum.java
+++ programmers/lv1/RemoveSmallestNum.java
@@ -1,31 +1,37 @@
-import java.util.Arrays;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class RemoveSmallestNum {
+    // 제일 작은 수 제거하기
+    // 2024-06-30
     public static int[] solution(int[] arr) {
+        // 배열의 원소가 하나밖에 없을 경우
+        if(arr.length == 1) return new int[]{-1};
         int[] answer = new int[arr.length - 1];
+        int min = arr[0];
 
-        if (arr.length > 1){
-            for (int i = 0; i < arr.length - 1; i++) {
-                if (i < arr.length - 1) {
-                    if (arr[i] < arr[i + 1]) {
-                        arr[i + 1] = arr[i];
-                    }
-                } else if (i == arr.length - 1) {
-                    if (arr[i] < arr[i + 1]) {
-                        answer[i] = arr[i];
-                    }
-                }
+        // 제일 작은 수 구하기
+        for (int i : arr) {
+            if (i < min){
+                min = i;
             }
         }
-        else {
-            answer[0] = -1;
+
+        // 제일 작은 수를 제외한 배열을 만듬
+        int index = 0;
+        for (int i : arr) {
+            if (i != min) {
+                answer[index] = i;
+                index ++;
+            }
         }
+
         return answer;
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4};
-        int[] answer = solution(arr);
-        System.out.println("arr = " + Arrays.toString(answer));
+        int[] arr = {4, 3, 2, 1};
+        System.out.println(Arrays.toString(solution(arr)));
     }
+
 }
