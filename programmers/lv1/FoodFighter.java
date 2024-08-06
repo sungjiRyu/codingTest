203f02564b317526b87f78ab3bf1de9250eaa342
sungjiRyu <dhftlcm@gmail.com>
2024-08-07 01:08:09 +0900

    푸드 파이트 대회

---
+++ programmers/lv1/FoodFighter.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class FoodFighter {
+    // 2024-08-06
+    // 푸드 파이트 대회
+    public static void main(String[] args) {
+        int[] food = {1, 3, 4, 6};
+        System.out.println("solution(food) = " + solution(food));
+    }
+    public String solution(int[] food) {
+        int q = 0;
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 1; i <= food.length - 1; i++) {
+            q = food[i] / 2;
+            for ( int j = 0; j < q; j++){
+                answer.append(i);
+            }
+        }
+
+        answer.append(0);
+
+        for (int i = food.length - 1; i > 0; i--) {
+            q = food[i] / 2;
+            for ( int j = 0; j < q; j++){
+                answer.append(i);
+            }
+        }
+
+        return answer.toString();
+    }
+}
