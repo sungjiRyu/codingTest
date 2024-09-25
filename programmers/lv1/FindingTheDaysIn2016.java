507bcee0b0cfdb824d7ec98e1c95e4b7812254f4
sungjiRyu <dhftlcm@gmail.com>
2024-09-25 23:11:49 +0900

    2016(라이브러리 없이 풀기)

--- programmers/lv1/FindingTheDaysIn2016.java
+++ programmers/lv1/FindingTheDaysIn2016.java
@@ -23,4 +23,33 @@ public class FindingTheDaysIn2016 {
     }
 
     // 라이브러리를 이용해서 푸는게 맞는걸까..
+    // solution2(라이브러리 사용 x)
+    public static String solution2(int a, int b) {
+        String answer = "";
+
+        String[] day = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
+        int[] monthCnt = {31,29,31,30,31,30,31,31,30,31,30,31,};
+
+        int allDate = 0;
+        int daysDifference = 0;
+
+        for (int i = 0; i < a - 1; i++) {
+            allDate += monthCnt[i];
+        }
+
+        allDate += b-1;
+
+        answer = day[allDate % day.length];
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String[] dayCount = {"31","29","31","30","31","30","31","31","30","31","30","31",};
+        int sum = 0;
+
+        System.out.println(solution2(1, 2));
+    }
+
+
 }
