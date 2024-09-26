2c60d6eb6137b7819434682641b9f41d69843e3a
sungjiRyu <dhftlcm@gmail.com>
2024-09-26 22:43:10 +0900

    기사단원의 무기(푸는중)

---
+++ programmers/lv1/KnightsWeapon.java
@@ -0,0 +1,21 @@
+public class KnightsWeapon {
+    // 2024-09-25
+    // 기사단원의 무기
+    public int solution(int number, int limit, int power) {
+        // number : 기사의 수
+        // limit : 공격력 제한수치
+        // power : 공격력 제한수치를 넘길경우 사용해야 하는 무기의 공격력
+        int answer = 0;
+
+        int[] weapon = new int[number];
+
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j < i; j++) {
+
+            }
+        }
+
+        return answer;
+    }
+
+}
